d1dadc601f759f09a4edd5b33c18d167d6812d4d
Angela Weber and Noah Horwitz <angela@>
2019-11-06 13:08:21 -0800

    update css

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-// BE LOGIC ------------
+// BACK END LOGIC ------------
 
 // PIG-DICE OBJECT:
 var pigDice = {
@@ -11,19 +11,20 @@ var switchMod = function(mod){
   switch (mod)
   {
     case 0:
-    // console.log("Turn # ",pigDice.turnCounter);
-    console.log("Player1's turn");
-
     var player1Score = pigDice.playerOneScore += diceRun;
     $("#player1Score").text(player1Score);
+    $('.playera').toggleClass("player1");
+    $('.playerb').toggleClass("player2");
+    $('.buttons').toggleClass("buttons2");
 
     break;
     default:
-    // console.log("Turn # ",pigDice.turnCounter);
-    console.log("Player2's turn")
-
     var player2Score = pigDice.playerTwoScore += diceRun;
     $("#player2Score").text(player2Score);
+    $('.playerb').toggleClass("player2");
+    $('.playera').toggleClass("player1");
+    $('.buttons').toggleClass("buttons2");
+
 
   }
 }
@@ -32,7 +33,10 @@ var diceRun = 0;
 
 function addDice(diceCount) {
   if(diceCount === 1){
-    console.log("Turnover");
+    $('.playera').toggleClass("player1");
+    $('.playerb').toggleClass("player2");
+    $('.buttons').toggleClass("buttons2");
+
     pigDice.turnCounter +=1
     diceRun = 0;
     alert("You rolled a 1, Turn over!")
@@ -56,7 +60,6 @@ $(document).ready(function() {
     var diceCount = Math.round(Math.random() * 5)+1
     $(".diceCount").append(diceCount, ", ")
     addDice(diceCount);
-    console.log("DiceRun = ",diceRun);
   });
 
 
@@ -65,7 +68,6 @@ $(document).ready(function() {
     pigDice.turnCounter +=1;
     switchMod(mod);
     diceRun = 0;
-    console.log("Turn# ",pigDice.turnCounter);
     $(".diceCount").text("");
     if(pigDice.playerOneScore >= 100){
       alert("Player1 Wins!");
@@ -73,26 +75,22 @@ $(document).ready(function() {
       diceRun = 0
       pigDice.playerTwoScore = 0;
       pigDice.playerOneScore = 0;
+      $("#player1Score").text("");
+      $("#player2Score").text("");
+
     } else if (pigDice.playerTwoScore >= 100) {
       alert('player2 Wins!');
       $(".diceCount").text("");
       diceRun = 0;
       pigDice.playerTwoScore = 0;
       pigDice.playerOneScore = 0;
+      $("#player1Score").text("");
+      $("#player2Score").text("");
     } else {
       return;
     }
 
-
-
-    // $("#player1Score").text(player1Score);
-    // var player1Score = pigDice.playerOneScore += diceRun;
-
-
    });
-
-
-
 });
 
 
